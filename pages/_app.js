028825ad117164cd9ce0f1bb7a8405bcedbec005
Admin <jithinks112@gmail.com>
2020-10-11 09:17:01 +0530

    Added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/main.css";
 import "antd/dist/antd.css";
 import "../styles/react-tags.css";
+import Favicon from "../src/components/favicon/index";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Favicon />
+      <Component {...pageProps} />
+    </>
+  );
 }
